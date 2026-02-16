f7f1f505886e5be7c80c84a9f313c3e988c917a3
StahilB <bukinstahil@gmail.com>
2026-02-16 20:43:59 +0000

    build

--- src/app/pages/order/order.ts
+++ src/app/pages/order/order.ts
@@ -3,6 +3,7 @@ import { Header } from "../../header/header";
 import { DELIVERY_SIZES, DELIVERY_SPEEDS } from './order.config';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UpperCasePipe } from '@angular/common';
+import { DeliveryApi } from '../../services/delivery-api';
 
 declare var ymaps: any;
 
@@ -24,9 +25,10 @@ export class Order {
 
   public orderId: any = signal(null);
   public calculationResult: any = signal(null);
+  public loading: any = signal(false);
 
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private deliveryApi: DeliveryApi) {
     this.routeForm = this.formBuilder.group({
       from: ['', Validators.required],
       to: ['', Validators.required],
@@ -68,6 +70,8 @@ export class Order {
     if (!this.map || this.routeForm.invalid) {
       return;
     }
+
+    this.loading.set(true);
 
     const { from, to, size, speed } = this.routeForm.getRawValue();
 
@@ -113,6 +117,7 @@ export class Order {
           total,
           speed
         });
+        this.loading.set(false);
       } catch (err) {
         this.failedCalculation();
       }
@@ -123,6 +128,7 @@ export class Order {
 
   private failedCalculation() {
     this.calculationResult.set(null);
+    this.loading.set(false);
     alert('Не удалось построить маршрут. Проверьте адреса и выбранные параметры.');
   }
 
@@ -148,8 +154,13 @@ export class Order {
       calculation: calculation,
       createdAt: new Date().toISOString()
     };
+    this.deliveryApi.createDelivery(payload).subscribe((response) => {
+      if ('error' in response) {
+        alert(response.error);
+        return;
+      }
 
-    console.log(payload);
-    this.orderId.set(1);
+      this.orderId.set(response.id);
+    });
   }
 }
